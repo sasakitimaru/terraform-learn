ef4c15afa375b2949ba912dc49a358fe981761b1
sasakitimaru <tom.ok1@icloud.com>
2024-06-17 22:36:58 +0900

    feat:差分検知してワークフローを管理

--- test.sh
+++
@@ -1,7 +0,0 @@
-if git diff --name-only HEAD^1 HEAD | grep -q '^deployment/init/'; then
-  echo "Deployment files have changed"
-  exit 0
-else
-  echo "Deployment files have not changed"
-  exit 1
-fi
